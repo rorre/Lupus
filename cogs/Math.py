cb54c4b8703fca7032eb3da3472f269db92569be
Rendy Arya Kemal <rendyarya22@gmail.com>
2020-04-29 11:00:07 +0700

    len()

--- cogs/Math.py
+++ cogs/Math.py
@@ -109,7 +109,7 @@ class Math(commands.Cog):
             except Exception as e:
                 response += f"{e.__class__.__name__} occured."
             i += 1
-        if response > 1994:
+        if len(response) > 1994:
             return await ctx.send("The result(s) requires more than 2000 characters. Aborting.")
         await ctx.send(f"```{response}```")
 
